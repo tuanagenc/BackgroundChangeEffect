0477df34b968737ff7e4a20fca21ed22d2bb24a8
tuanagenc028@gmail.com <tuanagenc028@gmail.com>
2024-03-21 15:22:23 +0300

    Background changing effect project is finished

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var ImgBox = document.querySelector(".img-box");
 var imgWrap = document.querySelector(".img-wrap");
 var originalImg = document.getElementById("originalImg");
+var line =document.getElementById("line");
 
 originalImg.style.width = ImgBox.offsetWidth + "px";
 
@@ -9,4 +10,5 @@ var leftSpace = ImgBox.offsetLeft;
 ImgBox.onmousemove = function (e) {
     var boxWidth = (e.pageX - leftSpace) + "px";
     imgWrap.style.width = boxWidth;
+    line.style.left = boxWidth;
 }
